5188520ae0c8643c09917b00bdaa9105c825750a
virelt <virelt@gmail.com>
2015-12-04 15:59:34 +0300

    header fill
    Added
    1) logo
    2) tagline
    3) filter
    4) dropdown menu + js opener

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,18 @@
 $(function () {
+
+	$('.menu__item').click(function(e){
+		e.preventDefault();
+		var current = $(this).parent('li');
+		if (current.hasClass('open')){
+			current.removeClass('open');
+		} else{
+			$(".menu li").removeClass('open');
+			current.toggleClass('open');
+		}
+
+		console.log(current.hasClass('open'))
+	});
+
 	var map;
 	map = new google.maps.Map(document.getElementById('map'), {
 	    center: {lat: -34.397, lng: 150.644},
